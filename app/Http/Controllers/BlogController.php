e5be8d6dacd396e871d5b5d9a7358a1790377d4a
enfant <enfantin@nate.com>
2018-08-31 14:05:36 +0900

    Show author name and post date in post excerpt and show pagination

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BlogController extends Controller
+{
+	protected $limit = 3;
+    public function index()
+    {
+    	
+    	$posts = Post::latest()->SimplePaginate($this->limit);
+
+    	return view('blog.index', compact('posts'));
+    	
+    }
+}
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title','body','excerpt'];
+
+
+    public function getImageUrlAttribute()
+    {
+    	$imageUrl="";
+    	if (! is_null($this->image)) {
+    		$image_path = public_path()."/img/".$this->image;
+    		if (file_exists($image_path)) {
+    			$imageUrl = asset("/img/".$this->image);
+
+    		}
+    	}
+        
+    	return $imageUrl;
+    }
+
+    public function getDateAttribute()
+    {
+    	return $this->created_at->diffForHumans();
+    }
+
+    public function author()
+    {
+    	return $this->belongsTo('App\User');
+    }
 }
